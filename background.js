e10e9281294c19d7f6707694251d2f82bca7482b
Amfonz <a_michael9@live.com>
2018-05-15 20:07:25 -0400

    core functionality complete styling left to go

--- background.js
+++ background.js
@@ -1,8 +1,18 @@
 function displayPage(){
-    var properties = {
-        active : true,
-        url : "main_page/tab_reduce.html"
-    }
-    browser.tabs.create(properties);
+    browser.tabs.query({currentWindow:true}).then((tabs)=>{
+        //if there is a tab reduce already open just make it active otherwise open a fresh one
+        let present = false;
+        for (let tab of tabs){
+            if(tab.title === "Tab Reduce"){ present = true;}
+        }
+        if(!present){
+            var properties = {
+                active : true,
+                url : "main_page/tab_reduce.html",
+                index : 0
+            }
+            browser.tabs.create(properties);
+        }
+    });
 }
 browser.browserAction.onClicked.addListener(displayPage);
--- main_page/tab_reduce_script.js
+++ main_page/tab_reduce_script.js
@@ -1,20 +1,23 @@
+//todo
+/*
 
-//store tab info in a global map we use this map to generate list of tabs that are not reduced
-//if the element is in the map add it, if reduced remove it
-//should get a message from the background script listening for new tabs and adding them to the list
-//if a tab is deleted same deal
-//
+*/
+
 
 //function to add to all reduce buttons
-function reduce_and_add_to_bottom(e){
+function reduceAndAddToBottom(e){
     //divToMove is the div containing the link and title and button to be put into the bottom list
-    var divToMove = document.getElementById(e.target.getAttribute("tabid"));
+    var divToMove = e.target.parentNode;
    document.getElementById("reduced-tabs-list").appendChild(divToMove);
-    browser.tabs.remove(parseInt(e.target.getAttribute("tabid")));
+    browser.tabs.remove(parseInt(divToMove.getAttribute("id")));
     e.target.remove();
 }
+function expandAndRemoveFromBottom(e){
+    var pNode = e.target.parentNode.parentNode;
+    pNode.removeChild(e.target.parentNode);
+}
 
-function reduce_all(){
+function reduceAll(){
     //loop through all tabs less the active tab and close and add to bottom
     let tabsInList = document.getElementById("tab-select-list");
     //check if it has any chilrend if not return
@@ -22,29 +25,17 @@ function reduce_all(){
         return;
     }
     let destination = document.getElementById("reduced-tabs-list");
-    let divsToReduce = Array.from(document.getElementsByClassName("link-div"));
-    for(var i = 0; i < divsToReduce.length;i++){
-        var cur = divsToReduce[i];
-        destination.appendChild(cur);
-        divsToReduce[i].lastChild.remove();
-        browser.tabs.remove(parseInt(cur.getAttribute("id")));
-    }
-    document.getElementById("reduce-all-button").innerHTML = "Expand All";
-    
-    
-}
-function expand_all(){
-    
-}
-function context_all(e){
-    e.target.innerHTML === "Expand All" ? expand_all() : reduce_all();
+    while(tabsInList.firstChild){
+        tabsInList.firstChild.lastChild.remove();
+        browser.tabs.remove(parseInt(tabsInList.firstChild.getAttribute("id")));
+        destination.append(tabsInList.firstChild);  
+    }  
 }
 
 
 
 
-//what is below should become an init function
-var allTabs = {};
+
 function getCurrentWindowTabs(){
     //return a list of tabs in the current window
     return browser.tabs.query({currentWindow:true});
@@ -52,15 +43,18 @@ function getCurrentWindowTabs(){
 function genTabList(){
     //run this function everytime we click on the tab for this page
     getCurrentWindowTabs().then((tabs)=>{
-        //get the area that we are dumping the tabs to
-        //we are putting them in a element containing the tabs title, url and a checkbox
-        //also include a hidden span that has the id of the tab
+        //tab-select-list is where the open tabs list is put
         let tabsList = document.getElementById("tab-select-list");
+        //clear the current list (if there is one) and make a new one
+        while(tabsList.firstChild){
+            tabsList.removeChild(tabsList.firstChild);
+        }
         let currentItem = document.createDocumentFragment();
         //loop through tabs if not active tab add to list
         for(let tab of tabs){
             if(!tab.active){
                 /**
+                structure of a list element
                 <div>
                     <p><tab.title></p>
                     <a href="<tab.url">tab.url</a>
@@ -77,20 +71,22 @@ function genTabList(){
                 tabData.setAttribute("id",tab.id);
                 tabData.classList.add("link-div");
                 
-                reduceButton.setAttribute("tabid",tab.id);
                 reduceButton.setAttribute("id","button"+tab.id);
                 reduceButton.classList.add("reduce-button");
                 reduceButton.textContent = "reduce";
                 reduceButton.addEventListener("click",function(event){
-                   reduce_and_add_to_bottom(event); 
+                   reduceAndAddToBottom(event); 
                 });
                 
                 tabTitle.textContent = tab.title;
+                tabTitle.classList.add("tab-titles");
                 
                 tabLink.setAttribute("href",tab.url);
                 tabLink.setAttribute("target","_blank");
                 tabLink.textContent=tab.url;
-                
+                tabLink.addEventListener("click",function(event){
+                   expandAndRemoveFromBottom(event); 
+                });
                 
                 tabData.appendChild(tabTitle);
                 tabData.appendChild(tabLink);
@@ -104,4 +100,11 @@ function genTabList(){
 }
 
 document.addEventListener("DOMContentLoaded",genTabList);
-document.getElementById("reduce-all-button").addEventListener("click",context_all);
+document.getElementById("reduce-all-button").addEventListener("click",reduceAll);
+browser.tabs.onActivated.addListener((tabInfo)=>{
+    browser.tabs.get(tabInfo.tabId).then((tab)=>{
+       if(tab.title === "Tab Reduce"){
+           genTabList();
+       }
+    });
+});
